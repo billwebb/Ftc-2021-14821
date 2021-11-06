8128e3a391a6f65aee4b565fc1c3fbafc7bde81b
Team14821 <team14821@decaturrobotics.org>
2021-11-06 11:10:59 -0400

    First meet, arm and driving, initial autonomous.

--- build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
+++ build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
@@ -8,5 +8,5 @@ public final class BuildConfig {
   public static final String LIBRARY_PACKAGE_NAME = "com.qualcomm.ftcrobotcontroller";
   public static final String BUILD_TYPE = "debug";
   // Field from default config.
-  public static final String APP_BUILD_TIME = "2021-11-04T18:00:51.755-0400";
+  public static final String APP_BUILD_TIME = "2021-11-06T09:59:10.820-0400";
 }
