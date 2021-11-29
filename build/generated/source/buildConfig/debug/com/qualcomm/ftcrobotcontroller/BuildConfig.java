e69b803ac7215f85be31aabf5bb108c08dc37b4c
Team14821 <team14821@decaturrobotics.org>
2021-11-29 16:15:14 -0500

    After second meet

--- build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
+++ build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
@@ -8,5 +8,5 @@ public final class BuildConfig {
   public static final String LIBRARY_PACKAGE_NAME = "com.qualcomm.ftcrobotcontroller";
   public static final String BUILD_TYPE = "debug";
   // Field from default config.
-  public static final String APP_BUILD_TIME = "2021-11-19T17:08:37.856-0500";
+  public static final String APP_BUILD_TIME = "2021-11-20T10:47:45.318-0500";
 }
